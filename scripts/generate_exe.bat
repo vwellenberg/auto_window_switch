a980af13e6b5c77510f6cdfb03213bf33ce5f6a4
vwellenberg <vwellenberg@googlemail.com>
2024-02-01 02:43:59 +0100

    Create generate_exe.bat

---
+++ scripts/generate_exe.bat
@@ -0,0 +1,2 @@
+conda activate ds & pyinstaller --noconfirm --onefile --distpath "" --console "src/main.py"
+@REM conda activate ds & pyinstaller --noconfirm --onefile --distpath "bin" --console "src/main.py"
