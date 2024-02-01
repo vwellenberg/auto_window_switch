2047a4c9d49828f47cd798d1430bf94c50b46b81
vwellenberg <vwellenberg@googlemail.com>
2024-02-01 18:00:33 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 from window_mgr import WindowMgr
+import logging
 
 # TODO tests
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        filename='logfile.log',
+        filemode='w',
+        encoding='utf-8',
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.DEBUG,
+        datefmt='%Y-%m-%d %H:%M:%S')
     ws = WindowMgr()
