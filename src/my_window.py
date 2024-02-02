f53f07d01b05f90fdee3c9621eb268c08abf60fb
vwellenberg <vwellenberg@googlemail.com>
2024-02-02 04:27:40 +0100

    Update my_window.py
    autopep8

--- src/my_window.py
+++ src/my_window.py
@@ -1,7 +1,10 @@
 import logging
-import win32gui, win32com.client, win32con
-import datetime 
-import traceback 
+import win32gui
+import win32com.client
+import win32con
+import datetime
+import traceback
+
 
 class MyWindow():
     def __init__(self, windowId: int, windowName: str) -> None:
@@ -15,9 +18,9 @@ class MyWindow():
     #     logging.debug(f'activateWindow {self.title}')
     #     try:
     #         win32gui.ShowWindow(self.id, win32con.SW_MAXIMIZE)
-    #     except: 
+    #     except:
     #         pass
-    #     try: 
+    #     try:
     #         windowHandel = win32gui.FindWindow(None, self.title)
 
     #         shell = win32com.client.Dispatch("WScript.Shell")
@@ -25,12 +28,13 @@ class MyWindow():
     #         win32gui.SetForegroundWindow(windowHandel)
     #         t = datetime.datetime.now().strftime("%H:%M:%S")
     #         print(f'{t} -- Activated "{self.title}"')
-    #     except: 
+    #     except:
     #         logging.debug(f'Window activation failed for "{self.title}" -- {traceback.format_exc()}')
     #     return True
 
+
 if __name__ == '__main__':
-    w1 = MyWindow(0,"bla1")
-    w2 = MyWindow(1,"bla2")
-    windows = [w1,w2]
+    w1 = MyWindow(0, "bla1")
+    w2 = MyWindow(1, "bla2")
+    windows = [w1, w2]
     print(windows)
