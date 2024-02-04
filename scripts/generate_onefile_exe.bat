f863cd46c8adc61de966e502cd56905ea156f6be
vwellenberg <vwellenberg@googlemail.com>
2024-02-04 14:46:34 +0100

    Update exe-gen batch files

--- scripts/generate_onefile_exe.bat
+++ scripts/generate_onefile_exe.bat
@@ -1,2 +1,2 @@
-conda activate ds & pyinstaller --noconfirm --console "src/main.py"
+conda activate ds & pyinstaller --noconfirm --onefile --distpath "" --console "src/main.py"
 @REM conda activate ds & pyinstaller --noconfirm --onefile --distpath "bin" --console "src/main.py"
